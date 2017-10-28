8c31ba981cb3716858c30c3144cc4e46c70179c2
Arles96 <arles.cerrato@unitec.edu>
2017-10-28 15:07:07 -0600

    add proyect

---
+++ BufferingCamposDelimitados/src/bufferingcamposdelimitados/BufferingCamposDelimitados.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bufferingcamposdelimitados;
+
+/**
+ *
+ * @author Arles Cerrato
+ */
+public class BufferingCamposDelimitados {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PrincipalWindow window = new PrincipalWindow("Hola");
+        window.setVisible(true);
+    }
+    
+}
